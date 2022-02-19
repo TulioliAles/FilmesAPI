223b16d3a96ef40dec404a963f25a66601fc6490
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-02-19 14:59:45 -0300

    Criação do projeto e do Model e Controller

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Titulo é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [StringLength(30, ErrorMessage = "O campo Gênero não pode ultrapassar 30 caracteres.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+
+        [Range(1, 200, ErrorMessage = "A Duração deve ter no mínimo 1 e no máximo 200 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
