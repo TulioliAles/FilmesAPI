c40c2a84403618bd7902cd3e360cef4364325f67
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-02-22 21:55:31 -0300

    Uso do contexto na Controller para persistir os dados no banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,28 +11,33 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
 
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+     
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+
+            _context.SaveChanges();
 
             return CreatedAtAction(nameof(RecuperaFilme), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperaFilme()
+        public IEnumerable<Filme> RecuperaFilme()
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmeById(int id)
         {
-            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if(filme != null)
             {
