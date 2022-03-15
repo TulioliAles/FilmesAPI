0eea0aedd5eda41aa6d2e2a1af38dab07c4b9dd1
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-03-14 22:16:34 -0300

    Criação Gerente

---
+++ FilmesAPI/Controllers/GerenteController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos.Gerente;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerenteController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public GerenteController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }       
+        
+        [HttpPost]
+        public IActionResult AdicionaGerente(CreateGerenteDto dto)
+        {
+            Gerente gerente = _mapper.Map<Gerente>(dto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperaGerentesPorId), new { Id = gerente.Id }, gerente);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaGerentesPorId(int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(gerente => gerente.Id == id);
+
+            if (gerente != null)
+            {
+                ReadGerenteDto filmeDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(filmeDto);
+            }
+
+            return NotFound();
+        }
+    }
+}
