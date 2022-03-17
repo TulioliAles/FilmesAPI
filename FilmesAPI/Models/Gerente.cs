ff5812105482862fc2b1ff3b18ae8d9d64312df0
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-03-16 21:31:48 -0300

    Criando relação entre Gerente e Cinema

--- FilmesAPI/Models/Gerente.cs
+++ FilmesAPI/Models/Gerente.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models
 {
@@ -8,5 +9,6 @@ namespace FilmesAPI.Models
         [Required]
         public int Id { get; set; }
         public string Nome { get; set; }
+        public virtual List<Cinema> Cinemas { get; set; }
     }
 }
